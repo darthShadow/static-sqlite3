76febee4e8d68f119d175c330fe5cb2a6e164dbb
Anagh Kumar Baranwal <6824881+darthShadow@users.noreply.github.com>
2022-12-12 15:15:23 +0530

    Build optimizations
    Signed-off-by: Anagh Kumar Baranwal <6824881+darthShadow@users.noreply.github.com>

--- build_static_sqlite.sh
+++ build_static_sqlite.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-SQLITE_ZIP_URL='https://www.sqlite.org/2022/sqlite-amalgamation-3390400.zip'
+SQLITE_ZIP_URL='https://www.sqlite.org/2022/sqlite-amalgamation-3400000.zip'
 SQLite_compressor='upx'  # Program to use for compressing compiled sqlite
                          # Keep it empty as "" to disable compression
 
